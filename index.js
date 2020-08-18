0e53a30f71b81cab686adfc2bba09c7f141a68d8
na_da <nadine.dalecki@gmail.com>
2020-08-18 13:32:51 +0000

    Glitch test

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const app = express()
 app.get("/", (request, response) => {
 	response.sendStatus(200)
 })
-app.listen(process.env.PORT)
+app.listen()
 
 /*╔═══════════════════════════════════════╗
     Dialogflow
